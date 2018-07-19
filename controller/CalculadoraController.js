f4bde0dedaf28ec494086e397434967d03550537
romulo-xavier25 <romulo.xavier25@gmail.com>
2018-07-19 16:15:25 -0300

    atualizando projeto calculadora, adicionado as operacoes nos botoes

--- controller/CalculadoraController.js
+++ controller/CalculadoraController.js
@@ -4,12 +4,47 @@ app.controller('CalculadoraController', function calculadora($scope) {
 	
 	$scope.operacaoSelecionada = null;
 	$scope.resultado = 0;
+	$scope.valorUm;
+	$scope.valorDois;
 
-	$scope.somar = function adicao(valor1, valor2) {
-		//this.resultado = valor1 + valor2;
-		var valorUm = parseInt(valor1);
-		var valorDois = parseInt(valor2);
-		this.resultado = valorUm + valorDois;
+	$scope.calcular = function calculadora(valor1, valor2) {
+		valorUm = parseInt(valor1);
+		valorDois = parseInt(valor2);
+
+		if ($scope.operacaoSelecionada == "+"){
+			this.resultado = valorUm + valorDois;
+		}
+		if ($scope.operacaoSelecionada == "-"){
+			this.resultado = valorUm - valorDois;
+		}
+		if ($scope.operacaoSelecionada == "/"){
+			if (valorDois <= 0) {
+				console.log("operacao invalida!");
+				this.resultado = "operacao invalida!"
+			}else{
+				this.resultado = valorUm / valorDois;
+			}
+		}
+		if ($scope.operacaoSelecionada == "*"){
+			this.resultado = valorUm * valorDois;
+		}
+
+	}
+
+	$scope.somar = function somando(){
+		$scope.operacaoSelecionada = "+";
+	}
+
+	$scope.subtrair = function subtraindo(){
+		$scope.operacaoSelecionada = "-";
+	}
+
+	$scope.multiplicar = function multiplicando(){
+		$scope.operacaoSelecionada = "*";
+	}
+
+	$scope.dividir = function dividindo(){
+		$scope.operacaoSelecionada = "/";
 	}
 
 
